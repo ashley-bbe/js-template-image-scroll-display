1327f45992059616f304f0db75c4dccd2318e357
New Author Name <rakotoson.ash.bbe@gmail.com>
2022-09-02 09:59:07 +0900

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+//ページのスクロールに合わせて画像が表示される
+
+window.addEventListener('scroll', reveal);
+
+function reveal() {
+  var reveals = document.querySelectorAll('.reveal');
+  var i;
+
+  for (i = 0; i < reveals.length; i++) {
+    if (reveals[i].getBoundingClientRect().top < window.innerHeight) {
+      reveals[i].classList.add('active');
+    } else {
+      reveals[i].classList.remove('active');
+    }
+  }
+}
+
+////////////////////////////////////////////
